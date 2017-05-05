8ec7100cdd38e6016305e1b8e94e946316fa4201
gabee1987 <gabee1987@gmail.com>
2017-05-05 12:08:54 +0200

    working on dict to file function

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+import os
+
+dir_path = os.path.dirname(os.path.realpath('request_counts.txt'))
 
 app = Flask(__name__)
+
+counts = {'GET': 0, 'POST': 0, 'DELETE': 0, 'PUT': 0}
 
 
 @app.route('/')
@@ -10,17 +15,25 @@ def index():
 
 @app.route('/request-counter', methods=['GET'])
 def request_counter_get():
-    return type(methods)
+    req_method = request.method
+    if req_method:
+        global counts
+        counts['GET'] += 1
+    return render_template('request_counter.html', req_method=req_method, counts=counts)
 
 
 @app.route('/request-counter', methods=['POST'])
 def request_counter_post():
-    pass
+    req_method = request.method
+    if req_method:
+        global counts
+        counts['POST'] += 1
+    return render_template('request_counter.html', req_method=req_method, counts=counts)
 
 
 @app.route('/statistics ', methods=['GET'])
 def request_counter_statistics():
-    pass
+    return render_template('statistics.html', req_method=req_method)
 
 
 if __name__ == '__main__':
